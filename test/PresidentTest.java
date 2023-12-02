8d01c2cc5e57c026beb919b3bdd0edf97e13f7f5
marialuiza <maria_luiza1598@hotmail.com>
2023-12-02 19:19:49 -0300

    AHH

--- test/PresidentTest.java
+++ test/PresidentTest.java
@@ -12,21 +12,8 @@ public class PresidentTest {
 
     President pres = new President(nome, party, number);
 
-    assertTrue("O nome do candidato nao esta como definido pelo builder.", pres.getName().equals(name));
-    assertTrue("O partido do candidato nao esta como definido pelo builder.", pres.getParty().equals(party));
-    assertTrue("O numero do candidato nao esta como definido pelo builder.", pres.getNumber() == number);
-  }
-
-  @Test
-  public void testPresidentConstructor(){
-    String name = "Alberto";
-    String party = "PV";
-    int number = 12;
-
-    President pres = new President(name, party, number);
-
-    assertEquals("O nome do candidato nao esta como definido pelo construtor.", name, pres.getName());
-    assertEquals("O partido do candidato nao esta como definido pelo construtor.", party, pres.getParty());
-    assertEquals("O numero do candidato nao esta como definido pelo construtor.", number, pres.getNumber());
+    Assert.assertTrue("O nome do candidato nao esta como definido pelo builder.", pres.getName().equals(name));
+    Assert.assertTrue("O partido do candidato nao esta como definido pelo builder.", pres.getParty().equals(party));
+    Assert.assertTrue("O numero do candidato nao esta como definido pelo builder.", pres.getNumber() == number);
   }
 }
