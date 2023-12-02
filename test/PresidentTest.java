ebffb9802ffe01e00bcc2b95d534277005e122e4
marialuiza <maria_luiza1598@hotmail.com>
2023-12-02 19:22:27 -0300

    FIXXING

--- test/PresidentTest.java
+++ test/PresidentTest.java
@@ -6,11 +6,12 @@ import org.junit.*;
 public class PresidentTest {
   @Test
   public void testPresidentBuilder(){
+
     String name = "Alberto";
     String party = "PV";
     int number = 12;
 
-    President pres = new President(nome, party, number);
+    President pres = new President(name, party, number);
 
     Assert.assertTrue("O nome do candidato nao esta como definido pelo builder.", pres.getName().equals(name));
     Assert.assertTrue("O partido do candidato nao esta como definido pelo builder.", pres.getParty().equals(party));
