4004cd8272bacf4660ece7bc16eb52aa0dd126a8
marialuiza <maria_luiza1598@hotmail.com>
2023-12-02 17:13:01 -0300

    adding gradle

--- src/test/java/PresidentTest.java
+++ src/test/java/PresidentTest.java
@@ -10,7 +10,7 @@ public class PresidentTest {
     String party = "PV";
     int number = 12;
 
-		President pres = new President.Builder()
+    President pres = new President.Builder()
         .name(name)
         .party(party)
         .number(number)
@@ -19,5 +19,18 @@ public class PresidentTest {
     assertTrue("O nome do candidato nao esta como definido pelo builder.", pres.getName().equals(name));
     assertTrue("O partido do candidato nao esta como definido pelo builder.", pres.getParty().equals(party));
     assertTrue("O numero do candidato nao esta como definido pelo builder.", pres.getNumber() == number);
-	}
+  }
+
+  @Test
+  public void testPresidentConstructor(){
+    String name = "Alberto";
+    String party = "PV";
+    int number = 12;
+
+    President pres = new President(name, party, number);
+
+    assertEquals("O nome do candidato nao esta como definido pelo construtor.", name, pres.getName());
+    assertEquals("O partido do candidato nao esta como definido pelo construtor.", party, pres.getParty());
+    assertEquals("O numero do candidato nao esta como definido pelo construtor.", number, pres.getNumber());
+  }
 }
