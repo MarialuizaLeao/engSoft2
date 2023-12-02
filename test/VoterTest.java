a0495862b55ae98111f31b840f516563bd758a16
marialuiza <maria_luiza1598@hotmail.com>
2023-12-02 20:46:43 -0300

    adding more voter tests

--- test/VoterTest.java
+++ test/VoterTest.java
@@ -23,10 +23,34 @@ public class VoterTest {
 			.state(state)
 			.build();
 
-		Assert.assertEquals("O número do título eleitoral não está como definido pelo builder.", electoralCard, voter.electoralCard);
-		Assert.assertEquals("O nome do eleitor não está como definido pelo builder.", name, voter.name);
-		Assert.assertEquals("O estado do eleitor não está como definido pelo builder.", state, voter.state);
+		Assert.assertEquals("O número do título eleitoral não está como definido pelo builder.", electoralCard, voter.getElectoralCard());
+		Assert.assertEquals("O nome do eleitor não está como definido pelo builder.", name, voter.getName());
+		Assert.assertEquals("O estado do eleitor não está como definido pelo builder.", state, voter.getState());
 	}
 
-  // Add more test cases as needed
+	@Test(expected = IllegalArgumentException.class)
+	public void testVoterBuilderWithNullElectoralCard() {
+		String electoralCard = null;
+		String name = "John Doe";
+		String state = "SP";
+
+		Voter voter = new Voter.Builder()
+			.electoralCard(electoralCard)
+			.name(name)
+			.state(state)
+			.build();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testVoterBuilderWithInvalidState() {
+		String electoralCard = "1234567890";
+		String name = "John Doe";
+		String state = "SJ";
+
+		Voter voter = new Voter.Builder()
+			.electoralCard(electoralCard)
+			.name(name)
+			.state(state)
+			.build();
+	}
 }
