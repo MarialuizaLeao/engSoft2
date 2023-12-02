d7fd2402b29224d345aa9e27328d139e1f878caf
marialuiza <maria_luiza1598@hotmail.com>
2023-12-02 20:01:30 -0300

    candidate test

--- test/CandidateTest.java
+++ test/CandidateTest.java
@@ -7,6 +7,17 @@ public class CandidateTest {
 
 	private Candidate candidate;
 
+	@Test
+	public void testCandidateBuilder() {
+		String name = "Alberto";
+		String party = "PV";
+		int number = 1232;
+		this.candidate = new Candidate(name, party, number);
+		Assert.assertTrue("O nome do candidato nao esta como definido pelo builder.", candidate.getName().equals(name));
+		Assert.assertTrue("O partido do candidato nao esta como definido pelo builder.", candidate.getParty().equals(party));
+		Assert.assertTrue("O numero do candidato nao esta como definido pelo builder.", candidate.getNumber() == number);
+	}
+
     @Test(expected = IllegalArgumentException.class)
     public void testCreateCandidateWithoutName() {
 		String name = null;
