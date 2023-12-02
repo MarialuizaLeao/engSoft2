0d7fa1c34832270bf24725ed0ce40db1edfa6ff4
marialuiza <maria_luiza1598@hotmail.com>
2023-12-02 19:52:48 -0300

    TEST

--- test/MayorTest.java
+++ test/MayorTest.java
@@ -5,10 +5,19 @@ import org.junit.*;
 
 public class MayorTest {
     @Test
-    public void testGetCity() {
-        Mayor mayor = new Mayor("John Doe", "Party A", 1, "State A", "City A");
-        String expected = "City A";
-        String actual = mayor.getCity();
-        Assert.assertEquals(expected, actual);
+    public void testMayorBuilder() {
+        String name = "John Doe";
+        String party = "Party A";
+        int number = 1;
+        String state = "MG";
+        String city = "City A";
+
+        Mayor mayor = new Mayor(name, party, number, state, city);
+        
+        Assert.assertTrue("O nome do candidato nao esta como definido pelo builder.", mayor.getName().equals(name));
+        Assert.assertTrue("O partido do candidato nao esta como definido pelo builder.", mayor.getParty().equals(party));
+        Assert.assertTrue("O numero do candidato nao esta como definido pelo builder.", mayor.getNumber() == number);
+        Assert.assertTrue("O estado do candidato nao esta como definido pelo builder.", mayor.getState().equals(state));
+        Assert.assertTrue("A cidade do candidato nao esta como definido pelo builder.", mayor.getCity().equals(city));
     }
 }
