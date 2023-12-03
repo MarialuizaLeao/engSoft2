3506aa5ab0c706f4aa855cea1c39a4c6b58b28dd
marialuiza <maria_luiza1598@hotmail.com>
2023-12-03 13:25:03 -0300

    changing

--- target/test-classes/system/test1.test.js
+++ target/test-classes/system/test1.test.js
@@ -17,7 +17,7 @@ describe('E2E Tests', () => {
         const {spawn, cleanup} = await prepareEnvironment();
         const {waitForText, getStdout, writeText, pressKey, wait, getExitCode, debug} = await spawn('java', '-jar ./target/Main.jar');
 
-        debug();   // enables logging to console from the tested program
+        //debug();   // enables logging to console from the tested program
 
         await expectText('(2) Entrar (TSE)', waitForText);
         await sendInput('2', writeText, pressKey);
